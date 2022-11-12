17b4c75a4f66cb06417218b44668c54bfd982f9a
lviano <v.luca96@fastwebnet.it>
2022-11-12 18:36:57 +0100

    12.11

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,14 @@
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>   // to remove before submission !!
+
+int	ft_printf(const char *, ...);
+
+int	main();
+
+#endif
