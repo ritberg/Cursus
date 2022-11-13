ad0298c64ee32579380cb25e05a0d6752917c990
Margarita Makarova <mmakarov@c2r3s2.42lausanne.ch>
2022-11-13 12:07:47 +0100

    13.11 ft_printf started but no idea

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,3 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakarov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 10:50:09 by mmakarov          #+#    #+#             */
+/*   Updated: 2022/11/13 12:06:46 by mmakarov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
 int	ft_printf(const char *, ...)
 {
+	const char	*s;
+	va_list	args;
+	int	i;
+
+	va_start(args, s);
+
+	while (*s)
+	{
+		if (*s == 'd')
+		{
+			i = va_arg(args, int);
+			ft_itoa(i);
+		}
+		s++;
+	}
+	va_end(args);
 }
