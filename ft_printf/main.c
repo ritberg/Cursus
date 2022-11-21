c0a6beeb0b1f6ee47feb34140e5a9df25c07738e
Margarita Makarova <mmakarov@c1r3s3.42lausanne.ch>
2022-11-21 15:00:01 +0100

    21.11

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: mmakarov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 10:50:48 by mmakarov          #+#    #+#             */
-/*   Updated: 2022/11/21 07:25:41 by mmakarov         ###   ########.fr       */
+/*   Updated: 2022/11/21 14:56:41 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main()
 {
-//	ft_printf("%d\n", 42);
-//	printf("Un nombre décimal (base 10): %d\n", 42);
+//	ft_printf("%d\n", 2147483647);
+//	printf("Un nombre décimal (base 10): %d\n", 2147483647);
 
 //	ft_printf("%u\n", -1); //unsigned is always pos !
 //	printf("Un nombre u: %u\n", -1);
@@ -32,29 +32,29 @@ int	main()
 //	int x = 1 + 2;
 //	char	*p = "Salut";
 //	ft_printf("%p\n", NULL);
-	printf("Pointeur: %p\n", NULL);
+//	printf("Pointeur: %p\n", NULL);
 	
-/*
-	Afficher les valeurs de retour pour x:
-	long l = 0;
+
+//	Afficher les valeurs de retour pour x:
+	long l = 25;
 
 	int r1 = ft_printf("%x", l);
 	int r2 = printf("%lx", l);
 	printf("\n R1 = %i, R2 = %i\n", r1, r2);
-*/
 
-//	ft_printf("%x\n", 103087);
-//	printf("Un nombre en hexadécimal (base 16) min: %x\n", 103087);
+/*
+	ft_printf("%x\n", 103087);
+	printf("Un nombre en hexadécimal (base 16) min: %x\n", 103087);
 
-//	ft_printf("%X\n", 103087);
-//	printf("Un nombre en hexadécimal (base 16) maj: %X\n", 103087);
+	ft_printf("%X\n", 103087);
+	printf("Un nombre en hexadécimal (base 16) maj: %X\n", 103087);
 	
 
-//	ft_printf("%%%%%%%\n");
-//	printf("\n%%%%%%%\n");
-
-//	ft_printf("salut\n");
-//	printf("salut\n");
+	ft_printf("%%%%%%%\n");
+	printf("\n%%%%%%%\n");
+*/
+	ft_printf("salut\n");
+	printf("salut\n");
 
 
 	return (0);
