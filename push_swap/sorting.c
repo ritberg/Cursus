cdfd5e3b16d599664421784f9455cf642c2e8d49
mmakarov <mmakarov@student.42lausanne.ch>
2022-12-19 18:45:57 +0100

    19.12

---
+++ push_swap/sorting.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 15:17:11 by mmakarov          #+#    #+#             */
+/*   Updated: 2022/12/19 16:42:00 by mmakarov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sorting.h"
+
+int	sorting(int *number)
+{
+	int	len_int;
+	int	longest_int;
+	char	*len_count;
+
+	longest_int = 1;
+	ft_itoa(number);
+	len_int = ft_strlen(len_count);
+	if (len_int > longest_int)
+		longest_int = len_int;
+
+}
