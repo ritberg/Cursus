6742a9fe9001d1ee0fe69387b37f2ba29891639e
lviano <v.luca96@fastwebnet.it>
2022-12-29 00:40:19 +0100

    28.12

--- push_swap/middle_sort.c
+++ push_swap/middle_sort.c
@@ -25,6 +25,7 @@ static	t_list	sort_pile_k(***pile_a, int size)
 				**pile_a = sa_sb(**pile_a, size, 'a');
 			**pile_a = (**pile_a)->next;
 		}
+		**pile_a = start;
 		size--;
 	}
 	return (pile_a);
@@ -38,13 +39,19 @@ void	middle_sort(**pile_a)
 	int	count;
 	int	middle;
 
+	/*
+	 find the number with index 25 in pile_k (it is key_nbr)
+	 se numero di pile_a <= key_nbr, metterlo in cima e pb
+	 (senza ordinare!)
+	*/
 	size = ft_lstsize(*pile_a);
 	pile_k = sort_pile_k(*pile_a, size);
-	key_nbr = ft_lstsize(pile_k) / 4;
+
+
 	while (size > 10 && size <= 100)
 	{
 		count = 1;
-		middle = key_nbr / 2;
+		middle = size / 2;
 		if (middle > key_nbr)
 		{
 			while (count <= key_nbr)
