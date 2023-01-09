a1de408ff01e94b7a888eef8cc3ce73681eee18d
mmakarov <mmakarov@student.42lausanne.ch>
2023-01-09 21:29:58 +0100

    09.01

--- push_swap_2/temp_libft.c
+++ push_swap_2/temp_libft.c
@@ -6,7 +6,7 @@
 /*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 12:33:38 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/01/09 15:50:18 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/01/09 21:27:12 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,41 +60,4 @@ int	ft_lstsize(t_list *lst)
 		len++;
 	}
 	return (len);
-}
-
-static char	*ft_strcat(char *dst, char *src)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (dst[i])
-		i++;
-	while (src[j])
-	{
-		dst[i] = src[j];
-		i++;
-		j++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-char *ft_strjoin(char const *s1, char const *s2)
-{
-	char	*dst;
-	char	*str1;
-	char	*str2;
-
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	if (str1 == 0 || str2 == 0)
-		return (0);
-	dst = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (dst == NULL)
-		return (0);
-	ft_strcpy(dst, str1);
-	ft_strcat(dst, str2);
-	return (dst);
 }
