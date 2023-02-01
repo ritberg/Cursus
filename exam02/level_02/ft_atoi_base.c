197432940392d3d80085332cbcf769603e06bd3a
lviano <v.luca96@fastwebnet.it>
2023-02-01 20:09:22 +0100

    1.02

--- exam02/level_02/ft_atoi_base.c
+++ exam02/level_02/ft_atoi_base.c
@@ -1,37 +1,27 @@
-int ft_atoi_base(const char *str, int str_base)
+int	ft_atoi_base(const char *str, int str_base)
 {
-    int i;
-    int count_sign;
-    int d;
-    int res;
-    char hexadec[100];
-    int j;
+	int	i;
+	int	sign;
+	int	result;
 
-    i = 0;
-    d = 0;
-    res = 0;
-    count_sign = 0;
-    j = 0;
-    while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
-        i++;
-    while (str[i] == '-')
-    {
-        count_sign++;
-        i++;
-    }
-    if (str_base = 16)
-        while (str[i])
-        {
-            d = d * 10 + (str[i] - '0');
-            i++;
-        }
-        while (d != 0)
-        {
-            res = d % 16;
-            if (res < 10)
-                hexadec[j] = res + '0';
-            else
-                hexadec[j] = res + 87;
-            d = d / 16;
-        }
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		result = result * str_base;
+		if (str[i] >= '0' && str[i] <= '9')
+			result = result + str[i] - '0';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			result = result + str[i] - '7';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			result = result + str[i] - 'W';
+		i++;
+	}
+	return (result * sign);
 }
