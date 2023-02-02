bd385da09d63941643172276e0e2a7749fda5f37
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-02 18:46:29 +0100

    02.-2

--- exam02/level_2/is_power_of_2.c
+++ exam02/level_2/is_power_of_2.c
@@ -1,20 +1,22 @@
 #include "stdio.h"
 
-int	is_power_of_2(unsigned int n)
+int is_power_of_2(unsigned int n)
 {
-	if (n == 0)
-		return (0);
-	if (n >= 0 && n % 2 == 0)
+	int nbr = 1;
+
+	while(nbr <= n)
 	{
-		is_power_of_2(n / 2);
-		return (1);
+		if (nbr == n)
+			return (1);
+		nbr = nbr * 2;
 	}
 	return (0);
 }
-
+/*
 int	main()
 {
-	printf("%d\n", is_power_of_2(128));
 	printf("%d\n", is_power_of_2(1));
+	printf("%d\n", is_power_of_2(6));
 	return (0);
 }
+*/
--- exam02/level_3/rstr_capitalizer.c
+++ exam02/level_3/rstr_capitalizer.c
@@ -1,36 +1,5 @@
 #include "unistd.h"
 
-int	main(int argc, char **argv)
-{
-	int	i;
-	int	j;
-
-	j = 1;
-	while (j < argc)
-	{
-		i = 0;
-		while (argv[j][i])
-		{
-			if (argv[j][i] >= 'A' && argv[j][i] <= 'Z')
-				argv[j][i] += 32;
-			if (i == 0 && argv[j][i] >= 'a' && argv[j][i] <= 'z')
-				argv[j][i] -= 32;
-			if ((argv[j][i + 1] == ' ' || argv[j][i + 1] == '\t') \
-					&& argv[j][i] >= 'a' && argv[j][i] <= 'z')
-				   argv[j][i] -=32;
-			if (argv[j][i + 1]== '\0' && argv[j][i] >= 'a' && argv[j][i] <= 'z')
-				argv[j][i] -=32;
-			write(1, &argv[j][i], 1);
-			i++;
-		}
-		write(1, "\n", 1);
-		j++;
-	}
-	return (0);
-}
-
-/* github
-
 void    rstr_capitalizer(char *str)
 {
     int i = 0;
@@ -65,4 +34,3 @@ int main(int argc, char **argv)
     return (0);
 }
 
-*/
