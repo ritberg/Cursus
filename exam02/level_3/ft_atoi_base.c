dbb1ebee89ef3b261260ddfcc7c473534b6a2871
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-03 17:37:05 +0100

    03.02

--- exam02/level_3/ft_atoi_base.c
+++ exam02/level_3/ft_atoi_base.c
@@ -1,27 +1,59 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int	ft_isspace(const char c)
+{
+	if (c == '\n' || c == '\t' || c == '\v' || c == '\r' || c == '\f')
+		return (1);
+	return (0);
+}
+
+int	base(char c, int base)
+{
+	char *str = "0123456789abcdef";
+	char *str2 = "0123456789ABCDEF";
+	int	 i = 0;
+
+	while (i < base)
+	{
+		if (c == str[i] || c == str2[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+
+}
+
 int	ft_atoi_base(const char *str, int str_base)
 {
+	int	res;
 	int	i;
 	int	sign;
-	int	result;
 
+	res = 0;
 	i = 0;
 	sign = 1;
-	result = 0;
-	if (str[i] == '-')
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign = -1;
 		i++;
 	}
-	while (str[i])
+
+	while (base(str[i], str_base) != -1)
 	{
-		result = result * str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			result = result + str[i] - '0';
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			result = result + str[i] - '7';
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			result = result + str[i] - 'W';
+		res = res * str_base;
+		res = res + base(str[i], str_base);
 		i++;
 	}
-	return (result * sign);
+	return (res * sign);
 }
+/*
+int	main()
+{
+	printf("%d\n", ft_atoi_base("Ceci permet de decouvrir le fonctionnement de ton ft_atoi_base.", atoi("16")));
+	return (0);
+}
+*/
