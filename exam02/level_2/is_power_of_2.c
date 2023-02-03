a57749a3fb6041e2bf557dd58f8b6f5b5715398a
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-03 14:03:53 +0100

    03.02

--- exam02/level_2/is_power_of_2.c
+++ exam02/level_2/is_power_of_2.c
@@ -2,6 +2,16 @@
 
 int is_power_of_2(unsigned int n)
 {
+	if (n == 0)
+		return (0);
+	while (n % 2 == 0)
+		n = n / 2;
+	return ((n == 1) ? 1 : 0);
+}
+
+/*
+int is_power_of_2(unsigned int n)
+{
 	int nbr = 1;
 
 	while(nbr <= n)
@@ -12,11 +22,13 @@ int is_power_of_2(unsigned int n)
 	}
 	return (0);
 }
-/*
+*/
+
+
 int	main()
 {
 	printf("%d\n", is_power_of_2(1));
 	printf("%d\n", is_power_of_2(6));
 	return (0);
 }
-*/
+
