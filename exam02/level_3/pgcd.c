4b74b51787d7d2ec26bf32a9ea6fba7090320f42
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-03 13:58:53 +0100

    03.02

--- exam02/level_3/pgcd.c
+++ exam02/level_3/pgcd.c
@@ -1,60 +1,34 @@
-#include "stdio.h"
 #include "stdlib.h"
+#include "stdio.h"
 
-int	main(int argc, char **argv)
-{
-	int	a = atoi(argv[1]);
-	int	b = atoi(argv[2]);
-	int	n;
-	if (a < b)
-		n = a;
-	else
-		n = b;
-	while (1)
-	{
-		if (a % n == 0 && b % n == 0)
-		{
-			printf("%d\n", n);
-			break;
-		}
-		--n;
-	}
-	return (0);
-}
-
-
-/* an other version
 int	main(int argc, char **argv)
 {
 	int	a;
 	int	b;
-	int	hcd;
-	int	count;
+	int	n;
 
-	count = 1;
+	if (argc != 3)
+	{
+		printf("\n");
+		return (0);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	if (argc == 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		if (a > 0 && b > 0)
+		if (a > b)
+			n = b;
+		else
+			n = a;
+		while (n > 0)
 		{
-			while (a != b)
+			if (a % n == 0 && b % n == 0)
 			{
-				if (a > b)
-				{
-					a = a - b;
-					printf("a %d\n", a);
-				}
-				else
-				{
-					b = b - a;
-					printf("b %d\n", b);
-				}
+				printf("%d\n", n);
+				break;
 			}
-			printf("answer  %d\n", a);
+			n--;
 		}
 	}
-	printf("\n");
 	return (0);
 }
-*/
