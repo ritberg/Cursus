6f2e0cedbba67e6e9daec12d39b88df1997b374e
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-04 17:04:51 +0100

    04.02

--- exam02/level_3/rstr_capitalizer.c
+++ exam02/level_3/rstr_capitalizer.c
@@ -1,36 +1,33 @@
 #include "unistd.h"
 
-void    rstr_capitalizer(char *str)
+int	main(int argc, char **argv)
 {
-    int i = 0;
+	int	i = 0;
+	int	j = 1;
 
-    while (str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] += 32;
-        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
-                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
-            str[i] -= 32;
-        write(1, &str[i++], 1);
-    }
+	if (argc <= 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	if (argc > 1)
+	{
+		while (argv[j])
+		{
+			i = 0;
+			while (argv[j][i])
+			{
+				if (argv[j][i] >= 'A' && argv[j][i] <= 'Z')
+					argv[j][i] +=32;
+				if (argv[j][i] >= 'a' && argv[j][i] <= 'z' && (argv[j][i+1] == ' ' \
+							|| argv[j][i+1] == '\t' || argv[j][i+1] == '\0'))
+					argv[j][i] -= 32;
+				write(1, &argv[j][i], 1);
+				i++;
+			}
+			write(1, "\n", 1);
+			j++;
+		}
+	}
+	return (0);
 }
-
-int main(int argc, char **argv)
-{
-    int i;
-
-    if (argc == 1)
-        write(1, "\n", 1);
-    else
-    {
-        i = 1;
-        while (i < argc)
-        {
-            rstr_capitalizer(argv[i]);
-            write(1, "\n", 1);
-            i++;
-        }
-    }
-    return (0);
-}
-
