e7208f571cb66dbdf9d48e2db6f5baccfc245eca
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-04 17:59:11 +0100

    04.02

--- exam02/level_2/snake_to_camel.c
+++ exam02/level_2/snake_to_camel.c
@@ -1,5 +1,56 @@
 #include "unistd.h"
 #include "stdlib.h"
+
+
+int	main(int argc, char **argv)
+{
+	int	i = 0;
+
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] == '_')
+			{
+				i += 1;
+				argv[1][i] -= 32;
+			}
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+
+/*
+int	main(int argc, char **argv)
+{
+	int	i;
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] != '_')
+				write(1, &argv[1][i], 1);
+			else
+			{
+				i += 1;
+				argv[1][i] -= 32;
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+*/
+
+
 /*
 int	main(int argc, char **argv)
 {
@@ -40,26 +91,3 @@ int	main(int argc, char **argv)
 }
 */
 
-int	main(int argc, char **argv)
-{
-	int	i;
-
-	i = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i])
-		{
-			if (argv[1][i] != '_')
-				write(1, &argv[1][i], 1);
-			else
-			{
-				i += 1;
-				argv[1][i] -= 32;
-				write(1, &argv[1][i], 1);
-			}
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
-}
