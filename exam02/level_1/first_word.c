bac562b22a65e9dd538fd252390aee1854183719
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-04 17:44:27 +0100

    04.02

--- exam02/level_1/first_word.c
+++ exam02/level_1/first_word.c
@@ -2,6 +2,28 @@
 
 int	main(int argc, char **argv)
 {
+	int	i = 0;
+
+	if (argc == 2)
+	{
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i++;
+		while (argv[1][i])
+		{
+			if (argv[1][i] != ' ' && argv[1][i] != '\t')
+				write(1, &argv[1][i], 1);
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				break;
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+/*
+int	main(int argc, char **argv)
+{
 	int	i;
 
 	i = 0;
@@ -21,3 +43,4 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+*/
