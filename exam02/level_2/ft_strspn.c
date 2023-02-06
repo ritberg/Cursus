e4c465ee255188ed812fabdda6d826013580b5c0
mmakarov <mmakarov@student.42lausanne.ch>
2023-02-06 19:08:35 +0100

    06.02

--- exam02/level_2/ft_strspn.c
+++ exam02/level_2/ft_strspn.c
@@ -4,6 +4,29 @@
 // return : how many letters are the same
 // with the main below answer is 3
 
+/*  it works but X3
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	size_t i = 0;
+	size_t	j = 0;
+	size_t	count = 0;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (accept[j] == s[i])
+				count++;;
+			j++;
+		}
+		i++;
+	}
+	return (count);
+}
+*/
+
+
 char	*ft_strchr(const char *accept, int c)
 {
 	while (*accept)
