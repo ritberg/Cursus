31357a66a8eb4946bd8fe8c995129460a88d3670
lviano <v.luca96@fastwebnet.it>
2023-02-25 13:32:45 +0100

    25.02

--- fractol/mandelbrot.c
+++ fractol/mandelbrot.c
@@ -1,13 +1,5 @@
 #include "fractol.h"
 
-void	my_mlx_pixel_put(t_fr *fr, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = fr->addr + (y * fr->line_length + x * (fr->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
 
 int	iteration(t_fr *fr)
 {
