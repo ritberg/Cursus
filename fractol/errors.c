8cf992044b25df3d381d3fb8976864d6ea51d112
mmakarov <mmakarov@student.42lausanne.ch>
2023-03-23 17:53:09 +0100

    23,03

--- fractol/errors.c
+++ fractol/errors.c
@@ -6,7 +6,7 @@
 /*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 19:33:02 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/03/20 11:38:03 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:46:41 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,35 @@ int	arg_check(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		if (!ft_strcmp(argv[1], "Mandelbrot"))
+		if (ft_strcmp(argv[1], "Mandelbrot") != 0)
 			return (0);
-		if (!ft_strcmp(argv[1], "Buddhabrot"))
+		if (ft_strcmp(argv[1], "Buddhabrot") != 0)
 			return (0);
 	}
-	if (argc == 4)
+	else if (argc == 4)
 	{
-		if (!ft_strcmp(argv[1], "Julia"))
+		if (!ft_strcmp(argv[1], "Julia") != 0)
 			return (0);
-		if (!ft_isdigit(argv[2]) && !ft_isdigit(argv[3]))
+		if (!ft_isdigit(argv[2]) || !ft_isdigit(argv[3]))
 			return (0);
-	/*	if (ft_atoi(argv[2]) > INT_MAX || ft_atoi(argv[2]) < INT_MIN || \
+		if (ft_atoi(argv[2]) > INT_MAX || ft_atoi(argv[2]) < INT_MIN || \
 				ft_atoi(argv[3]) > INT_MAX || ft_atoi(argv[3]) < INT_MIN)
 			return (0);
 		if (ft_atod(argv[2]) > DBL_MAX || ft_atod(argv[2]) < DBL_MIN || \
 				ft_atod(argv[3]) > DBL_MAX || ft_atod(argv[3]) < DBL_MIN)
 			return (0);
-	*/
 	}
+	else
+		return (0);
 	return (1);
 }
 
 int	errors(void)
 {
-	write(1, "Error! Valid params:\n", 21);
+	write(1, "Error! Example of valid params:\n", 32);
 	write(1, "-->Mandelbrot\n", 14);
 	write(1, "-->Buddhabrot\n", 14);
-	write(1, "-->Julia\n", 9);
+	write(1, "-->Julia -0.2 -0.8\n", 19);
 	exit(1);
 	return (0);
 }
