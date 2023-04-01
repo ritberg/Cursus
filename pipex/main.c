3b3098703f9f1991369d9bba14986ac061b23757
mmakarov <mmakarov@student.42lausanne.ch>
2023-04-01 19:17:59 +0200

    01.04

--- pipex/main.c
+++ pipex/main.c
@@ -2,19 +2,33 @@
 
 void    pipex(int f1, int f2)
 {
-    pipe(end);
+    int	nbytes;
+	char	*string = "Hello\n";
+	char	readbuffer[80];
+
+	pipe(end);
     parent = fork();
-    if (parent < 0)
+    if (parent == -1)
          return (perror("Fork: "));
-    if (!parent) // if fork() returns 0, we are in the child process
-        child_process(f1, cmd1);
+    if (parent == 0)
+	{
+		close(end[0]);
+		write(end[1], string, (strlen(string)+1));
+	}
     else
-        parent_process(f2, cmd2);
+	{
+		close(end[1]);
+		nbytes = read(end[0], readbuffer, sizeof(readbuffer));
+	}
 }
 int	main(int argc, char **argv)
 {
 	if (error_checker(argc, argv))
 		return (0);
+     f1 = open(argv[1], O_RDONLY);
+     f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+     if (f1 < 0 || f2 < 0)
+          return (-1);
 	pipex(f1, f2);
 	exit(0);
 	return (0);
