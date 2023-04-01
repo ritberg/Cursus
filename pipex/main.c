90f307589bd9482099f6bccb355c2dfcd91b3d52
mmakarov <mmakarov@student.42lausanne.ch>
2023-04-01 17:14:43 +0200

    01.04

--- pipex/main.c
+++ pipex/main.c
@@ -1,36 +1,21 @@
-#include "unistd.h"
-#include "stdio.h"
+#include "pipex.h"
 
-int	main()
+void    pipex(int f1, int f2)
 {
-	int	fd1[2];
-	int	fd2[2];
-	char	inputstr[100];
-
-	int	p;
-
-	scanf("%s", inputstr);
-	p = fork();
-
-	if (p > 0)
-	{
-		char	concatstr[100];
-
-		close(fd1[0]);
-		write(fd1[1], inputstr, strlen(inputstr) + 1);
-		close(fd1[1]);
-
-		wait(NULL);
-
-		close(fd2[1]);
-		read(fd2[0], concatstr, 100);
-		printf("Concatenated str %s\n", concatstr);
-		close(fd2[0]);
-	}
-	else
-	{
-		close(fd1[1]);
-		char	concatstr[100];
-
-		read(fd1[0], concatstr, 100);
+    pipe(end);
+    parent = fork();
+    if (parent < 0)
+         return (perror("Fork: "));
+    if (!parent) // if fork() returns 0, we are in the child process
+        child_process(f1, cmd1);
+    else
+        parent_process(f2, cmd2);
+}
+int	main(int argc, char **argv)
+{
+	if (error_checker(argc, argv))
+		return (0);
+	pipex(f1, f2);
+	exit(0);
+	return (0);
 }
