db1256695414322a8aed026595c8165ae506a4aa
mmakarov <mmakarov@student.42lausanne.ch>
2023-04-06 17:52:39 +0200

    06.04

--- pipex/main.c
+++ pipex/main.c
@@ -1,35 +1,82 @@
 #include "pipex.h"
 
-void    pipex(int f1, int f2)
+int	parsing(t_ppx *ppx, char **argv, char **envp) //
 {
-    int	nbytes;
-	char	*string = "Hello\n";
-	char	readbuffer[80];
-
-	pipe(end);
-    parent = fork();
-    if (parent == -1)
-         return (perror("Fork: "));
-    if (parent == 0)
-	{
-		close(end[0]);
-		write(end[1], string, (strlen(string)+1));
-	}
-    else
-	{
-		close(end[1]);
-		nbytes = read(end[0], readbuffer, sizeof(readbuffer));
-	}
+	ppx->path_envp = ft_substr(envp, 0, ft_strlen(argv[2]));
+	ppx->mypaths = ft_split(ppx->path_envp, ":");
+	ppx->mycmdargs = ft_split(argv[2], " ");
 }
-int	main(int argc, char **argv)
+
+int	parent_process(t_ppx *ppx, char **argv, char **envp)
 {
+	int	status;
+	int	i;
+
+	waitpid(-1, &status, 0); //
+	if (dup2() < 0)
+		return (perror("Error"));
+	dup2(ppx->f2, STDOUT_FILENO);
+	dup2(ppx->end[0], STDIN_FILENO);
+	close(ppx->end[1])
+	close(ppx->f2);
+	exit(EXIT_FAILURE);
+
+	i = -1;
+	while (ppx->mypaths[++i]) //
+	{
+		ppx->cmd2 = ft_join(ppx->mypaths[i], argv[3]);
+		exercve(ppx->cmd2, ppx->mycmdargs, envp);
+		free(ppx->cmd2);
+	}
+	return (EXIT_FAILURE);
+}
+
+int	child_process(t_ppx *ppx, char **argv, char **envp)
+{
+	int	i;
+
+	if (dup2() < 0)
+		return (perror("Error"));
+	dup2(ppx->f1, STDIN_FILENO);
+	dup2(ppx->end[1], STDOUT_FILENO);
+	close(ppx->end[0]);
+	close(ppx->f1);
+	exit(EXIT_FAILURE);
+
+	i = -1;
+	while (ppx->mypaths[++i]) //
+	{
+		ppx->cmd1 = ft_join(ppx->mypaths[i], argv[2]);
+		exercve(ppx->cmd1, ppx->mycmdargs, envp);
+		free(ppx->cmd1);
+	}
+	return (EXIT_FAILURE);
+}
+
+void	pipex(t_ppx *ppx, char **argv, char **envp)
+{
+	pipe(ppx->end);
+	ppx->parent = fork();
+	if (ppx->parent < 0)
+		return (perror("Fork: "));
+	if (!ppx->parent)
+		child_process(ppx, argv, envp); //child_process(f1, cmd1)
+	else
+		parent_process(ppx, argv, envp); //parent_process(f2, cmd2)
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_ppx	*ppx;
+	
+	ppx = malloc(sizeof(t_ppx));
 	if (error_checker(argc, argv))
 		return (0);
-     f1 = open(argv[1], O_RDONLY);
-     f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-     if (f1 < 0 || f2 < 0)
+     ppx->f1 = open(argv[1], O_RDONLY);
+     ppx->f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+     if (ppx->f1 < 0 || ppx->f2 < 0)
           return (-1);
-	pipex(f1, f2);
+	pipex(ppx, argv, envp);
 	exit(0);
 	return (0);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 17:04:54 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/04/01 19:04:06 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/04/06 17:33:36 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "unistd.h"
 # include "stdio.h"
+# include "stdlib.h"
 
 typedef struct s_ppx
 {
@@ -22,6 +23,12 @@ typedef struct s_ppx
 	int		f1;
 	int		f2;
 	int		end[2];
+	char	*cmd1;
+	char	*cmd2;
+
+	char	*path_envp;
+	char	**mypaths;
+	char	**mycmdargs;
 
 }	t_ppx;
 
