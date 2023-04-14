891fd50af5b5c7106639bd22e5b6ab0f7b41b20f
mmakarov <mmakarov@student.42lausanne.ch>
2023-04-14 12:08:28 +0200

    14.04

--- pipex/complicated_split_annexe.c
+++ pipex/complicated_split_annexe.c
@@ -6,57 +6,56 @@
 /*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:47:16 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/04/13 18:42:18 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:04:32 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**shortcut(char *src, char **dst, char c, int count) //pipex.h !
-{
-	int	j;
-
-	j = 0;
-	while (*src && *src != c)
-		dst[count][j++] = *src++;
-	dst[count++][j] = '\0';
-	return (dst);
-}
+// ./pipex infile.txt "grep -e 'a cat' -e 'k'" "wc -w" outfile.txt
 
 char	**ft_fill_big_table(char *src, char c, char **dst, int *lens)
 {
-//	int	j;
+	int	j;
 	int	count;
-	char	**res;
 
 	count = 0;
 	while (*src)
 	{
-		if (*src == c)
-			src++;
 		dst[count] = malloc(sizeof(char) * lens[count]);
 		if (dst[count] == NULL)
 			return (ft_free_split(dst, count));
-		free(lens); //
-		if (*src == 39)
+		j = 0;
+		if (*src == '\'' && src++)
+			while (*src != '\'')
+				dst[count][j++] = *src++;
+		else
+			while (*src && *src != c)
+				dst[count][j++] = *src++;
+		dst[count++][j] = '\0';
+		if (*src == c || *src == '\'')
+			src++;
+
+		/*
+		if (*src == '\'')
 		{
 			src++;
-		//	j = 0;
-		//	while (*src != 39)
-		//		dst[count][j++] = *src++;
-		//	dst[count++][j] = '\0';
-			res = shortcut(src, dst, 39, count);
+			j = 0;
+			while (*src != '\'')
+				dst[count][j++] = *src++;
+			dst[count++][j] = '\0';
 			break ;
 		}
 		else
 		{
-			//j = 0;
-			//while (*src && *src != c)
-			//	dst[count][j++] = *src++;
-			//dst[count++][j] = '\0';
-			res = shortcut(src, dst, c, index);
+			j = 0;
+			while (*src && *src != c)
+				dst[count][j++] = *src++;
+			dst[count++][j] = '\0';
 		}
+		*/
 	}
 	dst[count] = NULL;
-	return (res);
+	free(lens);
+	return (dst);
 }
