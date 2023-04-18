b4293e9ef1969194f747a08577dcf6fefb28af3e
mmakarov <mmakarov@student.42lausanne.ch>
2023-04-18 20:53:04 +0200

    18.04

--- pipex/complicated_split.c
+++ pipex/complicated_split.c
@@ -6,12 +6,42 @@
 /*   By: mmakarov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:47:31 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/04/14 12:06:54 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/04/18 20:51:54 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int	ft_n_words(char *str, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		if (*str != c)
+		{
+			if (*str == '\'' && str++)
+			{
+				count++;
+				while (*str && *str != '\'')
+					str++;
+				str++;
+			}
+			else
+			{
+				count++;
+				while (*str && *str != c)
+					str++;
+			}
+		}
+		else
+			str++;
+	}
+	return (count);
+}
+
+/*
 int	ft_n_words(char *str, char c)
 {
 	int	count;
@@ -38,7 +68,43 @@ int	ft_n_words(char *str, char c)
 	}
 	return (count);
 }
+*/
 
+int	*ft_len_strs(char *str, char c, int count, int index)
+{
+	int	*len_strs;
+	int	len;
+
+	len_strs = malloc(sizeof(int) * count);
+	if (len_strs == 0)
+		return (0);
+	while (*str)
+	{
+		len = 0;
+		if (*str == c)
+			str++;
+		while (*str && *str != c)
+		{
+			if (*str == '\'' && str++)
+			{
+				while (*str != '\'')
+				{
+					len++;
+					str++;
+				}
+			}
+			else
+			{
+				str++;
+				len++;
+			}
+		}
+		len_strs[index++] = len + 1;
+	}
+	return (len_strs);
+}
+
+/*
 int	*ft_len_strs(char *str, char c, int count, int index)
 {
 	int	*len_strs;
@@ -65,6 +131,7 @@ int	*ft_len_strs(char *str, char c, int count, int index)
 	}
 	return (len_strs);
 }
+*/
 
 char	**ft_free_split(char **dst, size_t i)
 {
@@ -86,6 +153,7 @@ char	**ft_splitpath(char *s, char c)
 
 	index = 0;
 	count = ft_n_words(s, c);
+	printf("%d\n", count);
 	lens = ft_len_strs(s, c, count, index);
 	dst = malloc(sizeof(char *) * (count + 1));
 	if (dst == NULL)
