3bcbbc7b7c55b01d5d9c5294c6571bd4e65f0964
mmakarov <mmakarov@student.42lausanne.ch>
2023-07-04 20:06:41 +0200

    04.07

--- Philosophers/philo/srcs/main.c
+++ Philosophers/philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mmakarov <mmakarov@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 15:52:22 by mmakarov          #+#    #+#             */
-/*   Updated: 2023/07/04 17:01:39 by mmakarov         ###   ########.fr       */
+/*   Updated: 2023/07/04 19:27:02 by mmakarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	main(int argc, char **argv)
 		return (1);
 	if (!join_threads_philos_checker(data))
 		return (1);
-//	mutex_destroy(data);
-//	free_structures(data);
+	mutex_destroy(data);
+	free_structures(data);
 	return (0);
 }
