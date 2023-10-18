8483be839750d125df291ae63e58d3a2e42ac35e
mmakarov <mmakarov@student.42lausanne.ch>
2023-10-18 12:34:39 +0200

    start cpp

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakarov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 11:55:59 by mmakarov          #+#    #+#             */
+/*   Updated: 2023/10/18 11:56:01 by mmakarov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    (void)argv;
+    
+    char    buff[512]
+
+    std::cout << "jjj" << std::endl;
+    std::cin >> buff;
+
+    return (0);
+}
